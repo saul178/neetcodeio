6af2cef1a46559b0b820d5f1e081e7f493551fe4
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-11-10 18:25:29 -0700

    merge sort algo

--- algorithms/sorting/main.go
+++ algorithms/sorting/main.go
@@ -24,3 +24,58 @@ func insertionSort(pairs []Pair) [][]Pair {
 
 	return res
 }
+
+func mergeSort(pairs []Pair) []Pair {
+	return mergeSortHelper(pairs, 0, len(pairs)-1)
+}
+
+func mergeSortHelper(pairs []Pair, start, end int) []Pair {
+	if end-start+1 <= 1 {
+		return pairs
+	}
+
+	mid := (start + end) / 2
+	mergeSortHelper(pairs, start, mid)
+	mergeSortHelper(pairs, mid+1, end)
+	merge(pairs, start, mid, end)
+
+	return pairs
+}
+
+func merge(pairs []Pair, start, mid, end int) {
+	// create the left and right arrays to length needed for them
+	leftArr := make([]Pair, mid-start+1)
+	copy(leftArr, pairs[start:mid+1])
+	rightArr := make([]Pair, end-mid)
+	copy(rightArr, pairs[mid+1:end+1])
+
+	// starting points for left and right array and the orginal array
+	i := 0
+	j := 0
+	k := start
+
+	// merge the two sorted halfs in the og array
+	for i < len(leftArr) && j < len(rightArr) {
+		if leftArr[i].Key <= rightArr[j].Key {
+			pairs[k] = leftArr[i]
+			i++
+		} else {
+			pairs[k] = rightArr[j]
+			j++
+		}
+		k++
+	}
+
+	// check if one of the halfs have any remaining elements to place into the og array
+	for i < len(leftArr) {
+		pairs[k] = leftArr[i]
+		i++
+		k++
+	}
+
+	for j < len(rightArr) {
+		pairs[k] = rightArr[j]
+		j++
+		k++
+	}
+}
--- algorithms/sorting/main_test.go
+++ algorithms/sorting/main_test.go
@@ -13,7 +13,7 @@ func TestInsertionSort(t *testing.T) {
 		expected [][]Pair
 	}{
 		{
-			name: "simple test",
+			name: "insertion sort test",
 			data: []Pair{{Key: 5, Value: "apple"}, {Key: 2, Value: "banana"}, {Key: 9, Value: "cherry"}},
 			expected: [][]Pair{
 				{{Key: 5, Value: "apple"}, {Key: 2, Value: "banana"}, {Key: 9, Value: "cherry"}},
@@ -26,6 +26,52 @@ func TestInsertionSort(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			actual := insertionSort(tt.data)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	tests := []struct {
+		name     string
+		data     []Pair
+		expected []Pair
+	}{
+		{
+			name: "merge sort stable test",
+			data: []Pair{
+				{Key: 5, Value: "apple"},
+				{Key: 2, Value: "banana"},
+				{Key: 9, Value: "cherry"},
+				{Key: 1, Value: "date"},
+				{Key: 9, Value: "elderberry"},
+			},
+			expected: []Pair{
+				{Key: 1, Value: "date"},
+				{Key: 2, Value: "banana"},
+				{Key: 5, Value: "apple"},
+				{Key: 9, Value: "cherry"},
+				{Key: 9, Value: "elderberry"},
+			},
+		},
+		{
+			name: "merge sort stable test 2",
+			data: []Pair{
+				{Key: 3, Value: "cat"},
+				{Key: 2, Value: "dog"},
+				{Key: 3, Value: "bird"},
+			},
+			expected: []Pair{
+				{Key: 2, Value: "dog"},
+				{Key: 3, Value: "cat"},
+				{Key: 3, Value: "bird"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := mergeSort(tt.data)
 			assert.Equal(t, tt.expected, actual)
 		})
 	}
